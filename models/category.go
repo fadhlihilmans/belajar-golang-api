757d3ec24ea90b63fc8083ce592456943efcc06b
fadhlihilmans <fadhli.hilmanss@gmail.com>
2026-05-04 15:25:38 +0700

    first

--- models/category.go
+++ models/category.go
@@ -1,6 +1,13 @@
 package models
 
-type Category struct {
-	Id   int    `json:"id" gorm:"primary_key"`
-	Name string `gorm:"type:varchar(100)" json:"name"`
+type Post struct {
+	Id         int      `json:"id" gorm:"primary_key"`
+	Title      string   `json:"title"`
+	Content    string   `json:"content"`
+	CategoryID uint     `json:"category_id"`
+	Category   Category `json:"category"`
 }
+
+// func (Category) TableName() string {
+//     return "kategori"
+// }
